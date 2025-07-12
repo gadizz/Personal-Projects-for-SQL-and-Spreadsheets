bfba46a8c8c598e3569eb0b75d5cf864208944ce
gadizz <gadizzreal@gmail.com>
2025-07-12 13:15:49 +0300

    Update and rename Personal project 2.sql to Using multiple aggregations.sql

--- Personal project 2.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-  AVG(UnitPrice) AS Avg_Unit_Price,
-  COUNT(DISTINCT(customerID)) AS num_of_customers
-FROM 
-  `inner-precept-464708-e9.Personal_projects.online retail`
---
+++ Using multiple aggregations.sql
@@ -0,0 +1,15 @@
+SELECT 
+  usertype,
+  start_station_name, 
+  end_station_name, 
+  CONCAT(start_station_name," to ", end_station_name) AS route,
+  COUNT(*) AS num_of_trips,
+  ROUND(AVG(CAST(tripduration AS INT64)/60), 2) AS duration
+FROM 
+  `bigquery-public-data.new_york_citibike.citibike_trips` 
+GROUP BY
+  start_station_name, end_station_name, usertype
+ORDER BY
+  num_of_trips DESC
+LIMIT
+  2000
