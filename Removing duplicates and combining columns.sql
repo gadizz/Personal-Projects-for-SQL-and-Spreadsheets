dc53877a4df041d5245ec850d97672d85c4be12a
gadizz <gadizzreal@gmail.com>
2025-07-12 13:45:29 +0300

    Update Removing duplicates and combining columns.sql

--- Removing duplicates and combining columns.sql
+++ Removing duplicates and combining columns.sql
@@ -4,4 +4,4 @@ FROM
   `inner-precept-464708-e9.Personal_projects.online retail`
 GROUP BY
   Customer_orders
--- The results are available in: Results/Result of using the Concat and distinct clause.csv
+-- The results are available in: Results/Result of using the Concat and Distinct clause.csv
