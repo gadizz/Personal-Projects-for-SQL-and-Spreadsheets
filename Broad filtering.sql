09ebbf7a21369d7b39c0fa7c0efb59fa64aad03d
gadizz <gadizzreal@gmail.com>
2025-07-12 13:11:15 +0300

    Update and rename Personal project 3.sql to Broad filtering.sql

---
+++ Broad filtering.sql
@@ -0,0 +1,12 @@
+SELECT 
+  * 
+FROM 
+  `bigquery-public-data.sdoh_cdc_wonder_natality.county_natality` 
+WHERE
+  County_of_Residence Like "%Niagara%"
+  OR County_of_Residence LIKE "%Erie%"
+  OR County_of_Residence LIKE "%Chautauqua%"
+ORDER BY
+  Births DESC
+LIMIT 
+  1000
--- Personal project 3.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  SUBSTR(Country, 1, 4) AS characters_in_country, Country
-FROM 
-  `inner-precept-464708-e9.Personal_projects.online retail`
-WHERE
-  Country LIKE "%Un%"
